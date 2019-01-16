aed37b674817945d68d6fa68c638c890d42ca549
MefitHp <mefitDev@gmail.com>
2019-01-16 15:38:46 -0600

    Added Project detail + Route

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetail from './components/projects/ProjectDetail';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Dashboard} />
+        <Route path="/project/:id" component={ProjectDetail} />
     </Switch>
 )
 
