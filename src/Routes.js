64bc9033b0af58a6bb9ef6cdf56e98a54aac940e
MefitHp <mefitDev@gmail.com>
2019-01-16 17:36:56 -0600

    Added Signin/Signup form + navbar links fixed

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetail from './components/projects/ProjectDetail';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route path="/project/:id" component={ProjectDetail} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
     </Switch>
 )
 
