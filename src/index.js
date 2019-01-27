974785e9c1015be1524fac433ca4343e0e872de4
MefitHp <mefitDev@gmail.com>
2019-01-26 21:10:38 -0600

    Added store enhancers, reduxFirestore and reactReduxFirebase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './redux/reducers/rootReducer'
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase'
+import { getFirestore, reduxFirestore } from 'redux-firestore'
+import firebaseConfig from './config/firebaseConfig'
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+        reduxFirestore(firebaseConfig),
+        reactReduxFirebase(firebaseConfig)
+    )
+)
 
 const WithRouter = () => (
     <BrowserRouter>
--- src/redux/actions/projectActions.js
+++ src/redux/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         //Make an async call
 
         dispatch({ type: 'CREATE_PROJECT', project: project })
